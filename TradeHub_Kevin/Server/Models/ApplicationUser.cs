451b0db16f82798a49238938fdac712343f43ed2
kevin <kevin@DESKTOP-6CIDVOQ>
2024-02-02 13:04:45 +0800

    Add project files.

---
+++ TradeHub_Kevin/Server/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TradeHub_Kevin.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
